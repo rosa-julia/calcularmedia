0e31391a4df20a76e61e259d16dd1ae5d73ee91b
rosa-julia <111510794+rosa-julia@users.noreply.github.com>
2022-09-01 09:51:20 -0300

    Add files via upload

---
+++ calcularrmedia.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+		float numero1, numero2, media;
+	printf("Digite o numero 1");
+	scanf("%f",&numero1);
+	printf("Digite o numero 2");
+	scanf("%f",&numero2);
+	media = (numero1+numero2)/2;
+	printf("%f",media);
+}
+
